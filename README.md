385946fcd5a9f38b68ef3727a984298bf249cb2d
tifrel <till.friesewinkel@googlemail.com>
2021-10-28 11:16:15 +0200

    Echo for multiple clients

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ This repo is a follow-along of
 
 ## Commits/iterations on the server
 
-1. A simple server that echoes what is being sent, and then exits
-2. A server that echoes until the client disconnects and then exits.
+1. A simple server that echoes what is being sent, and then exits.
+2. Echo until the client disconnects, then exit.
+3. Echo for multiple clients, never exit.
--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,30 @@ async fn main() {
     // set up TCP listener
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    // accept client connection
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
-    let (reader, mut writer) = socket.split();
-
-    let mut reader = BufReader::new(reader);
-    let mut line = String::new();
+    // this loops handles multiple clients
     loop {
-        // read packet
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 {
-            break;
-        }
-        // echo back
-        writer.write_all(line.as_bytes()).await.unwrap();
-        // flush buffer
-        line.clear();
+        // accept client connection
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        // we need to spawn/move here otherwise on client would block all the others
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            // this loops handles the packets sent by a single client
+            loop {
+                // read packet
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+                // echo back
+                writer.write_all(line.as_bytes()).await.unwrap();
+                // flush buffer
+                line.clear();
+            }
+        });
     }
 }
