be94601b8657d50306f7e7b0ea9d5c9334f1f659
tifrel <till.friesewinkel@googlemail.com>
2021-10-28 10:59:34 +0200

    Echo and exit server

---
+++ README.md
@@ -0,0 +1,6 @@
+This repo is a follow-along of
+[an awesome tutorial on async Rust + Tokio](https://www.youtube.com/watch?v=Iapc-qGTEBQ)
+
+## Commits/iterations on the server
+
+1. A simple server that echoes what is being sent, and then exits
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() {
+    // set up TCP listener
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    // accept cliend connection
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    // read packet
+    let mut buffer = [0u8; 1024];
+    let bytes_read = socket.read(&mut buffer).await.unwrap();
+    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+}
