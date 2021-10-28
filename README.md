7065a72c7349555e255db4d2586ec2763ce52e49
tifrel <till.friesewinkel@googlemail.com>
2021-10-28 11:49:36 +0200

    Chat server finished

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ This repo is a follow-along of
 1. A simple server that echoes what is being sent, and then exits.
 2. Echo until the client disconnects, then exit.
 3. Echo for multiple clients, never exit.
+4. An actual chat server: Every sent message gets broadcastet to all connected
+   clients.
+
+## Tokio concepts
+
+- `Future<T>`: Rust equivalent of a JS `Promise<T>`
+- Tasks: Lightweight threads (like goroutines in go, or coroutines)
+- `select!` macro: Like a `match`, but for futures. Executes the first future
+  that is resolved.
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,47 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     // set up TCP listener
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (tx, _rx) = broadcast::channel::<String>(1024);
 
     // this loops handles multiple clients
     loop {
         // accept client connection
         let (mut socket, _addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
-        // we need to spawn/move here otherwise on client would block all the others
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
 
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
 
-            // this loops handles the packets sent by a single client
             loop {
-                // read packet
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                //
+                tokio::select! {
+                    // getting a packet
+                    bytes_read = reader.read_line(&mut line) => {
+                        // empty packet -> disconnect
+                        if bytes_read.unwrap() == 0 {
+                            break;
+                        }
+                        // broadcast
+                        tx.send(line.clone()).unwrap();
+                        // flush buffer
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+                        writer.write_all(msg.as_bytes()).await.unwrap()
+                    }
                 }
-                // echo back
-                writer.write_all(line.as_bytes()).await.unwrap();
-                // flush buffer
-                line.clear();
             }
         });
     }
